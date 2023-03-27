c9f7d2b16c4e5606eb00e6edd8ad40857e790e17
Neil Dcruze <neildcruze@gmail.com>
2023-03-26 21:56:20 -0400

    Updates in Gradient Descent!

--- posts/gradient-descent/LogisticRegression.py
+++ posts/gradient-descent/LogisticRegression.py
@@ -38,8 +38,55 @@ class LogisticRegression:
             else: 
                 prev_loss = new_loss
         
-    def fit_stochastic(self, X, y, alpha, max_epochs, momentum = False):
-        return 0 
+    def fit_stochastic(self, X, y, alpha, max_epochs, batch_size, momentum = False):
+        #creating modified feature matrix X_ with column of 1s 
+        X_ = np.append(X, np.ones((X.shape[0],1)), axis=1)
+        
+        #setting the beta value of momentum - based on momentum True or False
+        beta = 0
+        if(momentum):
+            beta = 0.8
+        
+        #setting n as the number of rows - data points - in our feature matrix X_
+        n = X_.shape[0]
+        
+        #creating a temporary variable which stores the previous weight vector - useful for implementing momentum
+        prevWeightVec = 0
+        
+        #initializing previous loss as inf
+        prev_loss = np.inf
+        
+        #maximum number of time we are cycling through the data - max_epochs 
+        #np.arange is similar to range used in above fit function
+        for j in np.arange(max_epochs):
+            
+            #adding the score and loss history for the current weight vector
+            #this update cannot be included in the loop of the individual batches for consistency 
+            new_loss = self.loss(X_,y)
+            new_score = self.score(X_,y)
+            self.loss_history.append(new_loss)
+            self.score_history.append(new_score)
+            
+            #creating a list of 
+            order = np.arange(n)
+            np.random.shuffle(order)
+            
+            #this loop goes over the order - total number of rows (data points)
+            #and then creates batches out of those orders, where n (total data points) // batch_size + 1
+            for batch in np.array_split(order, n // batch_size + 1):
+                x_batch = X_[batch,:]
+                y_batch = y[batch]
+                #performing the stochastic gradient
+                gradient = self.gradient_logistic_loss(x_batch, y_batch)
+                tempWeightVec = self.w
+                self.w -= (alpha * gradient) + (beta * (self.w - prevWeightVec))
+                prevWeightVec = tempWeightVec
+            
+             #checking the conditions to terminate the loop - when overall improvement is minimum
+            if np.isclose(new_loss, prev_loss):
+                break
+            else: 
+                prev_loss = new_loss
     
     #defining related sigmoid function
     def sigmoid(self, z):
@@ -52,14 +99,14 @@ class LogisticRegression:
     #defining the gradient of the logistic loss function 
     def gradient_logistic_loss(self, X, y):
         #using the actual predictions - rather than 0s and 1s 
-        #to utilize the potential of the exponential loss function
+        #to utilize the potential of the logistic loss function
         y_hat = X@self.w
         return np.dot(self.sigmoid(y_hat) - y, X)/X.shape[0]
 
     #defining the empirical risk - Overall Loss - L(w) - function 
     def loss(self, X, y):
         #using the actual predictions - rather than 0s and 1s
-        #to utilize the potential of the exponential loss function 
+        #to utilize the potential of the logistic loss function 
         y_hat = X@self.w
         return self.logistic_loss(y_hat, y).mean()
                 
