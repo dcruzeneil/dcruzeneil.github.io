37e7b444d9b49d292cf69a7ad609cad852f11a6a
Neil Dcruze <neildcruze@gmail.com>
2023-05-01 21:17:54 -0400

    Updates!

--- posts/kernel-logistic-regression/KernelLogisticRegression.py
+++ posts/kernel-logistic-regression/KernelLogisticRegression.py
@@ -8,10 +8,11 @@ class KernelLogisticRegression:
         self.kernel_kwargs = kernel_kwargs
     
     def fit(self, X, y):
-        self.X_train = X
+        self.X_train = X 
         #computing the kernel matrix - which is an NxN matrix 
         km = self.kernel(X, X, **self.kernel_kwargs)
         #initializing random weight vector - v0
+        #v0 = np.random.rand(X_.shape[0])
         v0 = np.random.rand(X.shape[0])
         #minimize the self.loss function return value by adjusting the v paramater
         #for starters: take the v parameter as v0
@@ -19,7 +20,7 @@ class KernelLogisticRegression:
         self.v = result.x
         
     def predict(self, X):
-        km = self.kernel(self.X_train, X)
+        km = self.kernel(self.X_train, X, **self.kernel_kwargs)
         km_transpose = km.T
         return 1*((km_transpose@self.v)>0)
     
